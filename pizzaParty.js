ab5520f037266bb22205be11a131e63991e3845b
Jatin Nanda <jatin.nanda1337@gmail.com>
2015-10-17 23:17:18 -0400

    INITIAL PARSE STUFF WORKS

--- pizzaParty.js
+++ pizzaParty.js
@@ -1,8 +1,27 @@
+//DATABASE CLASS
+
+
+function createParse(name, availability, location) {
+	var TestObject = Parse.Object.extend("TestObject");
+	var testObject = new TestObject();
+	// testObject.save([{"name": name}, {"pizzaPref": pizzaPref}, {"location" : location}], {
+	testObject.save({"name": name}).then(function(object) {
+	  testObject.save({"availability": availability}).then(function(object) {
+	  	testObject.save({"location": location}).then(function(object) {
+	  		alert("it definitely worked!");
+	  		window.location = "PeopleSearch.html";
+	  	}
+	)})});
+}
+
+
 $("#submitButton").click(function () {
-    var name = $("#name").val();
+    var name = $("#name").val()
+    var location = $("#location option:selected").text();
     if (name == '') {
         alert("require name");
     } else {
-        alert(name);
+    	console.log(location);
+    	createParse(name, "available", location);
     }
 });
